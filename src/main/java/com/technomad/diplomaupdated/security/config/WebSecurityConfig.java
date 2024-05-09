10738679a3779f1bee9d54b09ca4683f533d0ba5
The Adventurer <leiurusrint@gmail.com>
2024-05-09 16:26:19 +0600

    Basic Auth added

--- src/main/java/com/technomad/diplomaupdated/security/config/WebSecurityConfig.java
+++ src/main/java/com/technomad/diplomaupdated/security/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -36,6 +37,7 @@ public class WebSecurityConfig {
                                 .loginPage("/login"))
                 .authenticationProvider(daoAuthenticationProvider());
 
+        http.httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
